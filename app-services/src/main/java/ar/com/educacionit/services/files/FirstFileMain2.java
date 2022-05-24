d126e522cddff5faeace8e44342f1f89265cf69d
carlos horacio lopez <krloss@gmail.com>
2022-05-23 22:00:00 -0300

    clase34
    - intro a File
    - intro a recursividad

---
+++ app-services/src/main/java/ar/com/educacionit/services/files/FirstFileMain2.java
@@ -0,0 +1,24 @@
+package ar.com.educacionit.services.files;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FirstFileMain2 {
+
+	public static void main(String[] args) throws IOException {
+		//crear un un objeto de la clase File
+		
+		File archivo = new File("c:/desarrollo");
+		
+		boolean isFile = archivo.isFile();
+		boolean isDirectory = archivo.isDirectory();
+		
+		System.out.println(isFile);
+		System.out.println(isDirectory);
+		
+		
+		//recursividad!
+		
+	}
+
+}
