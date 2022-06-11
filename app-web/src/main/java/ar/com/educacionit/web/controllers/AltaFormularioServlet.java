c2973b4cbe6b7fe8b223ce14918a2231c2965ba7
carlos horacio lopez <krloss@gmail.com>
2022-06-10 22:00:10 -0300

    clase41: etiquetas HTML, form, table, iframe, input

---
+++ app-web/src/main/java/ar/com/educacionit/web/controllers/AltaFormularioServlet.java
@@ -0,0 +1,26 @@
+package ar.com.educacionit.web.controllers;
+
+public class AltaFormularioServlet /*extends Alguien */{
+	
+	//GET
+	/*public void get(request, response) {
+		
+		//capturar los datos que se enviaron desde el form
+		
+		//aplica logica 
+		
+		//responder algo
+	}
+	
+	
+	//POST
+	//file
+	public void post(request, response) {
+		
+		//capturar los datos que se enviaron desde el form
+		
+		//aplica logica 
+		
+		//responder algo
+	}*/
+}
