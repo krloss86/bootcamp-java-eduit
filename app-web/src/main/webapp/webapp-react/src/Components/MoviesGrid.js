54c7ff92656fb3f5cedfd441cf4a8dd9b7423bbf
carlos horacio lopez <krloss@gmail.com>
2022-08-03 17:55:14 -0300

    add inifinte scroll

--- app-web/src/main/webapp/webapp-react/src/Components/MoviesGrid.js
+++ app-web/src/main/webapp/webapp-react/src/Components/MoviesGrid.js
@@ -5,39 +5,46 @@ import styles from './MovieGrid.module.css';
 import { get } from './../utils/httpClient';
 import { Spinner } from './Spinner';
 import { useQuery } from '../hooks/useQuery';
-export function MoviesGrid() {
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+export function MoviesGrid({search}) {
     //estado > steState
     const [peliculas, setPeliculas] = useState([]);
-    const [isLoading,setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
+    const [page,setPage] = useState(1);
+    const [hasMore,setHasMore] = useState(true);
 
-    const query = useQuery();
-    const search = query.get("search");
+    // const query = useQuery();
+    // const search = query.get("search");
 
     useEffect(() => {
         setIsLoading(true);
         const searchUrl = search
-         ? '/search/movie?query='+search
-         : '/discover/movie';
+            ? '/search/movie?query=' + search + "&page="+page
+            : '/discover/movie?page='+page;
 
-        
         get(searchUrl)
-        .then(data => {
-            setPeliculas(data.results)
-            setIsLoading(false);
-        });
-    },[search]);
+            .then(data => {
+                setPeliculas((prevMovies) => prevMovies.concat(data.results))
+                setIsLoading(false);
+                setHasMore(data.page < data.total_pages);
+            });
+    }, [search,page]);
 
-    if(isLoading) {
-        return <Spinner/>
-    }
-    
     return (
-        <ul className={styles.moviesGrid}> 
-            {
-                peliculas.map((movie) => {
-                    return <MovieCard key={movie.id} movie={movie}></MovieCard>
-                })
-            }
-        </ul>
+        <InfiniteScroll
+            dataLength={peliculas.length} //This is important field to render the next data
+            hasMore={true}
+            next={() => setPage((prevPage) => prevPage +1)}
+            loader={<Spinner />}
+        >
+            <ul className={styles.moviesGrid}>
+                {
+                    peliculas.map((movie) => {
+                        return <MovieCard key={Math.random() + movie.id} movie={movie}></MovieCard>
+                    })
+                    }
+                </ul>
+        </InfiniteScroll>
     );
 }
--- app-web/src/main/webapp/webapp-react/src/pages/LandingPage.js
+++ app-web/src/main/webapp/webapp-react/src/pages/LandingPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { MoviesGrid } from './../Components/MoviesGrid';
 import { Search } from './../Components/Search';
+import { useQuery } from './../hooks/useQuery';
 
 export function LandingPage() {
+    const query = useQuery();
+    const search = query.get("search");
+
     return (
         <>
             <Search/>
-            <MoviesGrid />
+            <MoviesGrid key ={search} search={search}/>
         </>
     )
 }
